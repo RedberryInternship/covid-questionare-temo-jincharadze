f3f40a21e4c00fca5b0a20f52284bd6fbcfd3a81
temito93 <temotemo161@gmail.com>
2022-12-20 15:30:46 +0400

    chore: add arrow button components

---
+++ src/components/forms/button/RightArrow.jsx
@@ -0,0 +1,21 @@
+const RightArrow = () => {
+  return (
+    <div>
+      <svg
+        width='14'
+        height='20.63'
+        viewBox='0 0 18 23'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M1 1L15 11.3158L1 21.6316'
+          stroke='#232323'
+          strokeWidth='2.4'
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default RightArrow;
