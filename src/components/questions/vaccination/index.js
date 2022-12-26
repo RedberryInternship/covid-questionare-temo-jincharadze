35be365f5fd52a5218fa3a64a7220c11dcb68b7f
temito93 <temotemo161@gmail.com>
2022-12-26 15:45:15 +0400

    refactor: index imports

--- src/components/questions/vaccination/index.js
+++
@@ -1,3 +0,0 @@
-export { default as VaccinationInfo } from './VaccinationInfo';
-export { default as useVaccinationInfo } from './useVaccinationInfo';
-export * from './notification';
--- src/components/questions/vaccination/notification/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Vaccinated } from './Vaccinated';
-export { default as NotVaccinated } from './NotVaccinated';
